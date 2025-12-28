bcc89903258bdd493ed81f9a387d7f44e6240c0c
TFournier37 <thomas.fourniercloutier@gmail.com>
2025-12-28 17:01:04 -0500

    ajout dun group_by

--- Devoir_1.R
+++ Devoir_1.R
@@ -1,4 +1,8 @@
 library(tidyverse)
 donnees <- read.csv("insurance.csv")
-donnees_filtrees <- donnees %>% filter(charges<50000) %>% filter (bmi<45)
+donnees_filtrees <- donnees %>% 
+  filter(charges<50000) %>% 
+  filter (bmi<45) %>% 
+  group_by(region)
+  
 
