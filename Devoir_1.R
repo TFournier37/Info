ea89b0d940bcda21f0062dd79e2cd2b0bd9402d6
TFournier37 <thomas.fourniercloutier@gmail.com>
2025-12-28 17:59:42 -0500

    enlever le group_by
    ne sert a rien

--- Devoir_1.R
+++ Devoir_1.R
@@ -2,8 +2,7 @@ library(tidyverse)
 donnees <- read.csv("insurance.csv")
 donnees_filtrees <- donnees %>% 
   filter(charges<50000) %>% 
-  filter (bmi<45) %>% 
-  group_by(region)
+  filter (bmi<45) 
 n <- nrow(donnees_filtrees)
 ind_rep <- sample (1:n,floor(.8*n))
 x <- donnees_filtrees %>% select(-charges)
